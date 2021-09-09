b4e4e2232b3d8b9e8c99abf1b37f7e76f3e693e9
Nebelwespe <78146117+Nebelwespe@users.noreply.github.com>
2021-09-09 13:21:44 -0500

    Add files via upload

---
+++ python.py
@@ -0,0 +1,52 @@
+# Lauren Scott
+#CSC 1200-002
+# Dr. Cyril Focht
+# 9/9/2021
+
+name = input("Enter name:")
+print("Name is: " + name)
+
+print("\n")
+
+
+address = input("Enter address:")
+print("Address is: " + address)
+
+print("\n")
+
+age = input("Enter age:")
+print("Age is: " + age)
+age = int(age)
+print("\n")
+
+number = input("Enter phone number:")
+print("Phone number is: " + number )
+
+print("\n")
+
+print("Your name is " + name + " and you are " , int(age) , " years old and you live at " + address + " and your phone number is " + number)
+
+print("\n")
+print("\n")
+
+name = input("Enter name:\n")
+
+print("\n")
+
+address = input("Enter address:\n")
+
+print("\n")
+
+age = input("Enter age:\n")
+age = int(age)
+
+print("\n")
+
+number = input("Enter phone number:\n")
+
+print("\n")
+
+print("Your name is " + name)
+print("You are", int(age) , "years old") 
+print("Your address is " + address )
+print("Your phone number is " + number)
